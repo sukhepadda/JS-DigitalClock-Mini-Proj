2d3560f5f557e3ad291565558776117df4fb28e4
sukhepadda <paddasukhwantsingh@gmail.com>
2023-01-22 21:43:27 +0530

    makes some changes

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // clock
+// 
 function clock(){
     let currDateTime = new Date();
-    let currTime = `${currDateTime.getHours()} : ${currDateTime.getMinutes()} : ${currDateTime.getSeconds()} : ${currDateTime.getMilliseconds()}`;
-    document.querySelector('#clock').innerText = currTime;
+    let clockHour = currDateTime.getHours();
+    let clockMin = currDateTime.getMinutes();
+    let clockSec = currDateTime.getSeconds();
+    let clockMiliSec = currDateTime.getMilliseconds();
+
+
+
+
+    // document.querySelector('#clock').innerText = currTime;
+    document.querySelector('#clockHour').innerText = clockHour;
+    document.querySelector('#clockMin').innerText = clockMin;
+    document.querySelector('#clockSec').innerText = clockSec;
+    document.querySelector('#clockMiliSec').innerText = clockMiliSec;
+    // document.querySelector('#clock').innerText = currTime;
+}
+function btnToggle(){
+    let switchToggle = document.getElementById('switch').value;
+if(switchToggle.checked = true ){
+    console.log('hello' )  
+}
+else {
+    console.log('nothing');
 }
 
+}
 setInterval(clock, 1);
