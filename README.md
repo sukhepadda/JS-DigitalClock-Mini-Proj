37cfda8faa07d3fcb3bf1b4d73b3e239d3b0d7ff
sukhepadda <paddasukhwantsingh@gmail.com>
2023-01-23 21:40:11 +0530

    style and hrs format done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JS-DigitalClock-Mini-Proj
 This is the mini proj for digital clock in js
+
+
+here is the codepen link: https://codepen.io/sukhepadda/full/poZaQza
--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 // clock
-// 
-function clock(){
-    let currDateTime = new Date();
-    let clockHour = currDateTime.getHours();
-    let clockMin = currDateTime.getMinutes();
-    let clockSec = currDateTime.getSeconds();
-    let clockMiliSec = currDateTime.getMilliseconds();
+//
+function clock24hrs() {
+  let currDateTime = new Date();
+  let clockHour = currDateTime.getHours();
+  let clockMin = currDateTime.getMinutes();
+  let clockSec = currDateTime.getSeconds();
+  let clockMiliSec = currDateTime.getMilliseconds();
 
-
-
-
-    // document.querySelector('#clock').innerText = currTime;
-    document.querySelector('#clockHour').innerText = clockHour;
-    document.querySelector('#clockMin').innerText = clockMin;
-    document.querySelector('#clockSec').innerText = clockSec;
-    document.querySelector('#clockMiliSec').innerText = clockMiliSec;
-    // document.querySelector('#clock').innerText = currTime;
+  // document.querySelector('#clock').innerText = currTime;
+  document.querySelector("#clockHour").innerText = clockHour;
+  document.querySelector("#clockMin").innerText = clockMin;
+  document.querySelector("#clockSec").innerText = clockSec;
+  document.querySelector("#clockMiliSec").innerText = clockMiliSec;
+  // document.querySelector('#clock').innerText = currTime;
 }
-function btnToggle(){
-    let switchToggle = document.getElementById('switch').value;
-if(switchToggle.checked = true ){
-    console.log('hello' )  
-}
-else {
-    console.log('nothing');
+function clock12hrs() {
+  let currDateTime = new Date();
+  let hour12 = (currDateTime.getHours() + 24) % 12 || 12;
+  //    console.log(hour12);
+
+  let clockHour = currDateTime.getHours();
+  let clockMin = currDateTime.getMinutes();
+  let clockSec = currDateTime.getSeconds();
+  let clockMiliSec = currDateTime.getMilliseconds();
+
+  // document.querySelector('#clock').innerText = currTime;
+  document.querySelector("#clockHour").innerText = hour12;
+  document.querySelector("#clockMin").innerText = clockMin;
+  document.querySelector("#clockSec").innerText = clockSec;
+  document.querySelector("#clockMiliSec").innerText = clockMiliSec;
+  if (clockHour > 12) {
+    // console.log('pm');
+    document.querySelector("#amPm").innerText = "PM";
+  } else {
+    // console.log('am');
+    document.querySelector("#amPm").innerText = "AM";
+  }
 }
 
+function btnToggle() {
+  let switchToggle = document.getElementById("switch");
+  if (switchToggle.checked == true) {
+    clearInterval(my24hrsInterval);
+    const my12hrsInterval = setInterval(clock12hrs, 1);
+  } else {
+    location.reload();
+  }
 }
-setInterval(clock, 1);
+const my24hrsInterval = setInterval(clock24hrs, 1);
